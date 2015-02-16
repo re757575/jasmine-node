821f6eb7aadd297a4e89c594ac8bf25aa6c9164f
re757575 <re757575@gmail.com>
2015-02-16 21:05:00 +0800

    init base example

---
+++ foo.js
@@ -0,0 +1,13 @@
+﻿
+var Foo = function(name, age) {
+    this.getName = function() { return name; };
+    this.setName = function(newName) { name = newName; };
+    this.getAge = function() { return age; };
+    this.setAge = function(newAge) { age = newAge; };
+}
+
+Foo.prototype.somePublicMethod = function() {
+    return this.getName();
+};
+
+module.exports = Foo;
---
+++ foo.spec.js
@@ -0,0 +1,16 @@
+﻿var Foo = require('./foo.js');
+
+describe("My Unit 測試", function() {
+
+    var foo = new Foo('Alex', '25');
+    it('should increment a variable', function () {
+	var result = foo.somePublicMethod();
+        expect(result).toEqual('Alex');
+    });
+
+    var foo = new Foo('Alex', '25');
+    it('should increment a variable', function () {
+	var result = foo.getAge();
+        expect(result).toBeGreaterThan(20);
+    });
+});
